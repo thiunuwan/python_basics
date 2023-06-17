b2235b454081266f09196865db272f0af354e3b1
thiunuwan <ravinduthiunuwan@gmail.com>
2023-06-17 14:02:35 +0530

    initial commit

---
+++ lesson2.py
@@ -0,0 +1,13 @@
+# variables
+name = "saman"
+print(name)
+
+a = 12j
+b = 12.5
+print(a)
+print(type(a))
+print(type(b))
+
+age = 15;
+print("hi "+name)
+print("age"+str(age))
---
+++ test.py
@@ -0,0 +1,11 @@
+# exponents
+print(2 ** 5)
+
+# division
+print(5 / 2)
+
+# floor division
+print(5 // 2)
+
+# module
+print(5 % 2)
