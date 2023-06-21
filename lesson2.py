f579af3357a7a17b5300c1644b500710ec0b2b74
thiunuwan <ravinduthiunuwan@gmail.com>
2023-06-21 11:53:55 +0530

    variables and data types  & builtin functions

--- lesson2.py
+++ lesson2.py
@@ -1,4 +1,4 @@
-# variables
+# variables and data types
 name = "saman"
 print(name)
 
@@ -11,3 +11,18 @@ print(type(b))
 age = 15;
 print("hi "+name)
 print("age"+str(age))
+print("his name is %s and his age is %d" %(name,age))
+price = 12.50
+print("price is %f"%price)
+print("price is %.2f"%price)
+
+
+# user input
+
+name = input("enter your name:")
+print("hi %s"%name)
+
+age = int(input("enter your age"))
+print(age)
+weight = float(input("enter your weight:"))
+print(weight)
---
+++ lesson3.py
@@ -0,0 +1,13 @@
+# builtin functions
+import datetime
+from math import sqrt
+
+print(pow(2,3))
+
+# print(help(pow))
+
+print(max(2,8,4,9,11,4,5,8,-25,4,10))
+print(min(2,8,4,9,11,4,5,8,-25,4,10))
+
+print(sqrt(144))
+print(datetime.datetime.now())
